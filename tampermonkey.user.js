6b53752ada58cc45cdfc628e925a10503e09d65c
bypass-api <thailanfreefire2000@gmail.com>
2025-08-24 15:40:22 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         EarnMate for Browser (Novelah/XWorld helper)
+// @namespace    http://tampermonkey.net/
+// @version      1.2.0
+// @description  Quản lý app, nhiệm vụ, ghi thu nhập, báo cáo & nhắc việc—chạy ngay trong trình duyệt (Tampermonkey)
+// @author       By R2lx
+// @require      https://raw.githubusercontent.com/lacongai/Tool/refs/heads/main/EarnMate.js
+// @match        https://novelah.com/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @grant        GM_addStyle
+// ==/UserScript==
